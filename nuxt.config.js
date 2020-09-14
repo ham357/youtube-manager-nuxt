bcc853619b8dfcd3d22340d6580b65546baa59eb
ham357 <sakuraniumarete@gmail.com>
2020-09-15 00:16:15 +0900

    変数名の修正

--- nuxt.config.js
+++ nuxt.config.js
@@ -74,7 +74,7 @@ export default {
   ** See https://axios.nuxtjs.org/options
   */
   axios: {
-    baseUrl: process.env.BASE_URL || 'http://localhost:8080/'
+    baseURL: process.env.BASE_URL || 'http://localhost:8080/'
   },
 
   env: {
